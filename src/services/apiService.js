1170e1662dc8cf4903854d73fe2d19fdac6e4798
andré <andredds12@gmail.com>
2021-07-02 23:31:45 -0300

    add url

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://mocha-pie.herokuapp.com";
 const BASE_API = "/api";
 
 export const save = (data) => axios.post(`${BASE_URL}${BASE_API}/`, data);
